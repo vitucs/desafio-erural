1e9f6fe58ccfadc9f212422ab526e89a35c0e300
Victor Santos <victorkclic@hotmail.com>
2023-03-09 17:15:39 -0300

    sqlite to pg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+    private
+
+    def record_not_found
+        render file: "#{Rails.root}/public/404.html", status: :not_found
+    end
 end
